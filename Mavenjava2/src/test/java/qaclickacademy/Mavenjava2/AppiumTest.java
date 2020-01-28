99542f6be4e7dbf87d3771a306c49dfdd23d34d2
ZANY <ZANY@ZANY-PC>
2020-01-27 21:24:07 -0500

    changes made by rajvir Jan 27 9:23am

--- Mavenjava2/src/test/java/qaclickacademy/Mavenjava2/AppiumTest.java
+++ Mavenjava2/src/test/java/qaclickacademy/Mavenjava2/AppiumTest.java
@@ -3,7 +3,7 @@ package qaclickacademy.Mavenjava2;
 import org.testng.annotations.Test;
 
 public class AppiumTest {
-
+//making changes to application to class
 	@Test
 	public void AppiumAutomation() {
 		
